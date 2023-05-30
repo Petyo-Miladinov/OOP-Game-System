5d8f8907185f117cb498cf18af29c6a60560bef4
Petyo <petyo.p.miladinov.2019@elsys-bg.org>
2023-05-30 23:50:06 +0300

    Mapper

--- src/main/java/elsys/oopprojectgame/mappers/BossMapper.java
+++ src/main/java/elsys/oopprojectgame/mappers/BossMapper.java
@@ -1,5 +1,23 @@
 package elsys.oopprojectgame.mappers;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import elsys.oopprojectgame.controllers.resources.BossResource;
+import elsys.oopprojectgame.entity.Boss;
+
+@Mapper
 public interface BossMapper {
-    
+    BossMapper BOSS_MAPPER = Mappers.getMapper(BossMapper.class);
+
+    @Mapping(target = "id", source = "id")
+    Boss fromBossResource(BossResource bossResource); 
+
+    @Mapping(target = "id", source = "id")
+    BossResource toBossResource(Boss boss);
+
+    List<BossResource> toBossResourceList(List<Boss> bosses);
 }
